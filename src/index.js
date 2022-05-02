bd46a33a1e8a2b0e653484ea0409fb10eafd2f44
aarabmed <aarab.mohamed90@gmail.com>
2022-05-02 00:53:04 +0000

    google analytics has been added

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import reportWebVitals from './reportWebVitals';
 import "./styles/styles.scss"
 
 
+const setGA = () => {
+  ReactGA.initialize(process.env.TRACKID);
+  ReactGA.pageview('Init page view');
+};
 
 const apolloClient = new ApolloClient({
   link: new HttpLink({
@@ -36,4 +40,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(setGA());
